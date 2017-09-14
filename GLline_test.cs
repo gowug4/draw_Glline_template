a0abb660d0d5f93b0fd65a04cfd159f012cfe296
GoWuG4 <B10102017@mail.ntust.edu.tw>
2017-09-14 16:34:32 +0800

    draw_Glline_template.git

---
+++ GLline_test.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GLline_test : MonoBehaviour
+{
+    private Material lineMaterial; 
+    public Color[] colors = new Color[6] { Color.blue, Color.cyan, Color.green, Color.magenta, Color.red, Color.yellow };
+
+    void Awake()
+    {
+        lineMaterial = new Material(Shader.Find("Custom/GizmoShader"));
+    }
+
+    void OnPostRender()
+    {
+        GL.PushMatrix();
+        lineMaterial.SetPass(0);
+        GL.LoadOrtho();
+        GL.Begin(GL.LINES);
+        for (int i = 0; i < 12; i++)
+        {
+            GL.Color(colors[i % 6]);
+            GL.Vertex3(0, i / 12f, 0);
+            GL.Vertex3(1, i / 12f, 0);
+        }
+        GL.End();
+        GL.PopMatrix();
+    }
+
+    void OnApplicationQuit()
+    {
+        DestroyImmediate(lineMaterial);
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
